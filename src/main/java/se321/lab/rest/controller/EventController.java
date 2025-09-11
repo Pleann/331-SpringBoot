0d5bc761b2ba9461098b62f5bdd7192eb09a3b0b
Plean Somjit <plean_som@cmu.ac.th>
2025-09-12 00:52:16 +0700

    3.3

--- src/main/java/se321/lab/rest/controller/EventController.java
+++ src/main/java/se321/lab/rest/controller/EventController.java
@@ -1,9 +1,12 @@
 package se321.lab.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import se321.lab.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -113,22 +116,20 @@ public class EventController {
                 .organizer("Chris Brown")
                 .build());
     }
-    @GetMapping("events")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = " _limit ",required=false) Integer perPage
-            ,@RequestParam(value = "_page",required = false) Integer page) {
-        perPage = perPage == null?eventList.size():perPage;
-        page = page == null?1:page;
-        Integer firstIndex = (page - 1) * perPage;
-        List<Event> output = new ArrayList<>();
-
-        try {
-            for (int i = firstIndex; i < firstIndex + perPage; i++) {
-                output.add(eventList.get(i));
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+        Event output = null;
+        for (Event event : eventList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
             }
-        } catch (IndexOutOfBoundsException ex) {
-            return ResponseEntity.ok(output);
         }
-        return ResponseEntity.ok(output);
+        if (output != null) {
+            return ResponseEntity.ok(output);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
     }
 
 }
