5ae84769e4c05c488da9b0c77598b9c9013d2bc0
Plean Somjit <plean_som@cmu.ac.th>
2025-09-12 02:14:19 +0700

    4.4

--- src/main/java/se321/lab/Application.java
+++ src/main/java/se321/lab/Application.java
@@ -19,6 +19,8 @@ public class Application {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/events")
                         .allowedOrigins("http://localhost:5173");
+                registry.addMapping("/events/*")
+                        .allowedOrigins("http://localhost:5173");
             }
         };
     }
