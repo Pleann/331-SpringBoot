0ac310dd6b4e5cdc3326d4ed93a5c3cc5286e6df
Plean Somjit <plean_som@cmu.ac.th>
2025-09-26 01:26:42 +0700

    lab8 6.2

--- src/main/java/se321/lab/rest/controller/OrganizerController.java
+++ src/main/java/se321/lab/rest/controller/OrganizerController.java
@@ -6,10 +6,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import se321.lab.entity.Event;
 import se321.lab.entity.Organizer;
 import se321.lab.service.OrganizerService;
 
@@ -40,5 +39,11 @@ public class OrganizerController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
+    }
+
+    @PostMapping("/organizers")
+    public ResponseEntity<?> addOrganizer(@RequestBody Organizer organizer) {
+        Organizer output = organizerService.save(organizer);
+        return ResponseEntity.ok(output);
     }
 }
