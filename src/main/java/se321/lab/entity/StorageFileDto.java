e012de77043656fef77d90c547be19d001ceacef
Plean Somjit <plean_som@cmu.ac.th>
2025-10-17 04:34:38 +0700

    lab 11 4.7

---
+++ src/main/java/se321/lab/entity/StorageFileDto.java
@@ -0,0 +1,10 @@
+package se321.lab.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StorageFileDto {
+    String name;
+}
---
+++ src/main/java/se321/lab/rest/controller/SupabaseController.java
@@ -0,0 +1,38 @@
+package se321.lab.rest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import se321.lab.entity.StorageFileDto;
+import se321.lab.util.SupabaseStorageService;
+
+
+@RestController
+@RequiredArgsConstructor
+public class SupabaseController {
+    final SupabaseStorageService supabaseStorageService;
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileUrl = supabaseStorageService.uploadFile(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error uploading file: " + e.getMessage());
+        }
+    }
+    @PostMapping("/uploadImage")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) {
+        try {
+            StorageFileDto fileUrl = supabaseStorageService.uploadImage(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(500)
+                    .body("Error uploading file: " + e.getMessage());
+        }
+    }
+}
